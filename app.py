bc0720fb0d199430abf52b3820b7f801d8ecfd60
TElphee01 <telpheemail@aol.com>
2021-01-23 15:30:09 -0500

    save word

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jha.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class Todo(db.Model): #Delete when new DB is finished
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
@@ -16,13 +16,31 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
         #Function returns a string everytime a new element is created.(returns ID of that task)
 
+class Jha(db.Model): #replace nullable = false on last few fields
+    JHAid = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(50))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    activityDescription = db.Column(db.String(200))
+    hazardDescription = db.Column(db.String(200))
+    hazardControl = db.Column(db.String(200))
+    steps = db.relationship('Step', backref='jha', lazy=True)
+
+    def __repr__(self):
+        return '<Jha %r>' % self.JHAid
+
+class Step(db.Model):
+    Stepid = db.Column(db.Integer, primary_key=True)
+    stepDescription = db.Column(db.String(200), nullable=False)
+    JHAid_id = db.Column(db.Integer, db.ForeignKey('jha.JHAid'),
+        nullable=False)
+
 @app.route('/home')# creates route to home page
 def home():
-        return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/newHazard')#Creates route to new hazard page (index.html)
 def newHazard():
-        return render_template('index.html')    
+    return render_template('index.html')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -40,33 +58,48 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/postJHA', methods=['POST', 'GET'])#route to post JHA
+def author():
+    if request.method =='POST':
+        jha_content = request.form['author']
+        new_jha = Jha(author=jha_content)
+
+        try:
+            db.session.add(new_jha)
+            db.session.commit()
+            return redirect('/postJHA')
+        except: # pylint: disable=bare-except
+            return "error adding Job Hazard Analysis"
+    else:
+        jha = Jha.query.order_by(Jha.date_created).all()
+        return render_template('index.html', jha=jha)
+
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    JHA_to_delete = Jha.query.get_or_404(id)
 
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(JHA_to_delete)
         db.session.commit()
-        return redirect('/')
+        return redirect('/postJHA')
     except: # pylint: disable=bare-except
-        return "Problem deleting task."
+        return "Problem deleting Job Hazard Analysis."
 
 
-@app.route('/update/<int:id>', methods=['GET', 'POST']) 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    JHA_to_update = Jha.query.get_or_404(id)
 
     if request.method == 'POST':
-        task.content = request.form['content']
+        Jha.author = request.form['author']
         #This sends current tasks content to form of the update box
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect('/postJHA')
         except: # pylint: disable=bare-except
-            return "Error updating task"
+            return "Error updating Job Hazard Analysis"
     else:
-        return render_template('update.html', task=task)
-
+        return render_template('update.html', jha=JHA_to_update)
 
 if __name__ =="__main__":
     app.run(debug=True)
