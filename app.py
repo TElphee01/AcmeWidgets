960649632a5be91282e4e11a1360006c4f376262
TElphee01 <telpheemail@aol.com>
2021-01-25 12:20:08 -0500

    full hazard function and some steps

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jha.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = b'JobHazardAnalysis101'
 db = SQLAlchemy(app)
-
-class Todo(db.Model): #Delete when new DB is finished
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-        #Function returns a string everytime a new element is created.(returns ID of that task)
 
 class Jha(db.Model): #replace nullable = false on last few fields
     JHAid = db.Column(db.Integer, primary_key=True)
@@ -26,7 +18,7 @@ class Jha(db.Model): #replace nullable = false on last few fields
     steps = db.relationship('Step', backref='jha', lazy=True)
 
     def __repr__(self):
-        return '<Jha %r>' % self.JHAid
+        return f"Jha('{self.JHAid}','{self.author}','{self.activityDescription}','{self.hazardDescription}','{self.hazardControl}')"
 
 class Step(db.Model):
     Stepid = db.Column(db.Integer, primary_key=True)
@@ -38,36 +30,30 @@ class Step(db.Model):
 def home():
     return render_template('home.html')
 
-@app.route('/newHazard')#Creates route to new hazard page (index.html)
-def newHazard():
-    return render_template('index.html')
+@app.route('/toSteps', methods=['POST', 'GET'])# creates route to steps page
+def toSteps():
+    stepQuery = Step.query.order_by(Step.JHAid_id).all()
+    return render_template('steps.html', steps=stepQuery)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/hazardList', methods=['POST', 'GET'])# creates route to list of Hazards
+def hazardL():
+    jha = Jha.query.order_by(Jha.date_created).all()
+    return render_template('hazards.html', jha=jha)
+
+@app.route('/', methods=['POST', 'GET'])#route to post JHA
 def index():
     if request.method =='POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
+        jha_author = request.form['author']
+        jha_activityDescription = request.form['activityDescription']
+        jha_hazardDescription = request.form['hazardDescription']
+        jha_hazardControl = request.form['hazardControl']
+        bulkJhaInsert =Jha(author=jha_author, activityDescription=jha_activityDescription, hazardDescription=jha_hazardDescription, hazardControl=jha_hazardControl)
+       
         try:
-            db.session.add(new_task)
+            db.session.add(bulkJhaInsert)
             db.session.commit()
+            flash('Job Hazard Analysis form added succesfully')
             return redirect('/')
-        except: # pylint: disable=bare-except
-            return "error adding task"
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
-
-@app.route('/postJHA', methods=['POST', 'GET'])#route to post JHA
-def author():
-    if request.method =='POST':
-        jha_content = request.form['author']
-        new_jha = Jha(author=jha_content)
-
-        try:
-            db.session.add(new_jha)
-            db.session.commit()
-            return redirect('/postJHA')
         except: # pylint: disable=bare-except
             return "error adding Job Hazard Analysis"
     else:
@@ -81,7 +67,7 @@ def delete(id):
     try:
         db.session.delete(JHA_to_delete)
         db.session.commit()
-        return redirect('/postJHA')
+        return redirect('/hazardList')
     except: # pylint: disable=bare-except
         return "Problem deleting Job Hazard Analysis."
 
@@ -91,16 +77,36 @@ def update(id):
     JHA_to_update = Jha.query.get_or_404(id)
 
     if request.method == 'POST':
-        Jha.author = request.form['author']
+        JHA_to_update.author = request.form['author']
+        JHA_to_update.activityDescription = request.form['activityDescription']
+        JHA_to_update.hazardDescription = request.form['hazardDescription']
+        JHA_to_update.hazardControl = request.form['hazardControl']
+
         #This sends current tasks content to form of the update box
         try:
             db.session.commit()
-            return redirect('/postJHA')
+            return redirect('/hazardList')
         except: # pylint: disable=bare-except
             return "Error updating Job Hazard Analysis"
     else:
         return render_template('update.html', jha=JHA_to_update)
 
+@app.route('/steps/<int:id>', methods=['GET', 'POST'])
+def steps(id):
+    if request.method =='POST':
+        step_stepDescription = request.form['stepDescription']
+        stepInsert =Step(stepDescription=step_stepDescription)
+       
+        try:
+            db.session.add(stepInsert)
+            db.session.commit()
+            flash('Job Hazard Step inserted successfully!')
+            return redirect('/toSteps')
+        except: # pylint: disable=bare-except
+            return "error adding Job Hazard step"
+    else:
+        stepQuery = Step.query.order_by(Step.JHAid_id).all()
+        return render_template('steps.html', steps=stepQuery)
+
 if __name__ =="__main__":
     app.run(debug=True)
-    
